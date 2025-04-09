be12f22c072139e6ebeea90b11050c15091881d1
AndresFelipeMorales <andresortiz1121341@correo.itm.edu.co>
2025-04-08 20:46:27 -0500

    Últimos cambios antes de reemplazar main

--- HankoSpa/obj/Debug/net8.0/HankoSpa.AssemblyInfo.cs
+++ HankoSpa/obj/Debug/net8.0/HankoSpa.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HankoSpa")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1232f916ad7fed49d4b6fc0f44f8ab566713d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0eea9ce4a9488acf5e0fb3809db7f4bfe79dd436")]
 [assembly: System.Reflection.AssemblyProductAttribute("HankoSpa")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HankoSpa")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
