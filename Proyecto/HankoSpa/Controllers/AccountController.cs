1ca064094d88665396a42f8f4e2961d38cc47d64
Czapata24 <cristianzapata1115966@correo.itm.edu.co>
2025-05-21 13:03:46 -0500

    funcional

---
+++ Proyecto/HankoSpa/Controllers/AccountController.cs
@@ -0,0 +1,73 @@
+﻿using HankoSpa.DTOs;
+using HankoSpa.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HankoSpa.Controllers
+{
+    public class AccountController: Controller
+    {
+        private readonly IUserService _userService;
+
+        public AccountController(IUserService usersService)
+        {
+            _userService = usersService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDTO dto)
+        {
+            if (ModelState.IsValid)
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _userService.LoginAsync(dto);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError(string.Empty, "Email o contraseña incorrecta");
+            }
+            return View(dto);
+
+        }
+
+        [HttpGet]
+        [Route("/Errors/{statuscode:int}")]
+
+        public IActionResult Error(int statuscode)
+        {
+            string errorMessage = "Ha ocurrido un error";
+            switch (statuscode)
+            {
+                case StatusCodes.Status401Unauthorized:
+                    errorMessage = "Debes iniciar sesion";
+                    break;
+                case StatusCodes.Status403Forbidden:
+                    errorMessage = "No tienes permiso para estar aqui";
+                    break;
+                case StatusCodes.Status404NotFound:
+                    errorMessage = "La pagina que estas intentando acceder no existe";
+                    break;
+            }
+            ViewBag.ErrorMessage = errorMessage;
+            return View(statuscode);
+        }
+        [HttpGet]
+        public IActionResult NotAuthorized()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            await _userService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+
+        }
+    }
+}
---
+++ Proyecto/HankoSpa/Data/Seeder/UserRolesSeeder.cs
@@ -0,0 +1,91 @@
+﻿using HankoSpa.Core;
+using HankoSpa.Models;
+using HankoSpa.Services.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace HankoSpa.Data.Seeder
+{
+    public class UserRolesSeeder
+    {
+        private readonly AppDbContext _context;
+        private readonly IUserService _userServices;
+
+        public UserRolesSeeder(AppDbContext context, IUserService usersService)
+        {
+            _context = context;
+            _userServices = usersService;
+        }
+
+        public async Task SeedAsync()
+        {
+            await CheckRoles();
+            await CheckUsers();
+        }
+
+        private async Task CheckUsers()
+        {
+            //Admin
+            User? user = await _userServices.GetUserAsync("cristian@gmail.com");
+
+            if (user is null)
+            {
+                CustomRol customRol = await _context.CustomRoles.FirstOrDefaultAsync(c => c.NombreRol == Env.SUPERADMINROLENAME);
+
+                user = new User
+                {
+                    Email = "cristian@gmail.com",
+                    FirstName = "Cristian",
+                    LastName = "Zapata",
+                    PhoneNumber = "3012025",
+                    UserName = "cristian@gmail.com",
+                    Document = "12121",
+                    CustomRol = customRol,
+                    Citas = null
+                };
+                var re = await _userServices.AddUserAsync(user, "123456");
+
+                string token = await _userServices.GenerateEmailConfirmationTokenAsync(user);
+                await _userServices.ConfirmEmailAsync(user, token);
+            }
+        }
+
+        private async Task CheckRoles()
+        {
+            await AdminRoleAsync();
+            await ContentManagerRoleAsync();
+        }
+
+        private async Task ContentManagerRoleAsync()
+        {
+            bool exists = await _context.CustomRoles.AnyAsync(c => c.NombreRol == "gestor");
+
+            if (!exists)
+            {
+                CustomRol role = new CustomRol { NombreRol = "Gestor" };
+                await _context.CustomRoles.AddAsync(role);
+
+                List<Permission> permissions = await _context.Permissions.Where(p => p.Module == "Secciones").ToListAsync();
+
+                foreach (Permission permission in permissions)
+                {
+                    await _context.RolPermissions.AddAsync(new RolPermission { Permission = permission, CustomRol = role });
+                }
+
+                await _context.SaveChangesAsync();
+            }
+
+        }
+
+        private async Task AdminRoleAsync()
+        {
+            bool exists = await _context.CustomRoles.AnyAsync(c => c.NombreRol == Env.SUPERADMINROLENAME);
+
+            if (!exists)
+            {
+                CustomRol role = new CustomRol { NombreRol = Env.SUPERADMINROLENAME };
+                await _context.CustomRoles.AddAsync(role);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
